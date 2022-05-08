11d70f160d8b04a74b4c811c46f8ffa83c60e5a7
Ammar Sayed <ammarsayed1988@gmail.com>
2022-05-08 02:08:40 +0200

    added smooth scrolling functionality

--- Project_02_Omnifood/js/script.js
+++ Project_02_Omnifood/js/script.js
@@ -11,3 +11,31 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
+
+//////////////////////////////////////////
+//smooth scrolling animation
+
+document.body.addEventListener('click', e => {
+  if (e.target.hasAttribute('href')) {
+    e.preventDefault();
+    const href = e.target.getAttribute('href');
+
+    if (href === '#') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+
+    //check if the href starts with # and length
+    if (href.startsWith('#') && href.length > 1) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    //close the nav menu if was link in nav-link was clicked
+    if (e.target.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  }
+});
